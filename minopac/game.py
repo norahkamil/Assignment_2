12cecf0ab360180241cefdf09654d2c4de98f765
norahkamil <norkam1971@gmail.com>
2025-12-17 20:51:09 +0100

    game.py

--- minopac/game.py
+++ minopac/game.py
@@ -200,7 +200,11 @@ class Game:
 
 
         #
-        # TODO: Draw the Ghosts here
+        # Draw a placeholder ghost at (13, 11)
+        ghost_surface = get_ghost_sprite((255, 0, 0))
+        ghost_pos = self.maze.grid_to_pixel((13, 11))
+        ghost_rect = ghost_surface.get_rect(center=ghost_pos)
+        self.screen.blit(ghost_surface, ghost_pos)
         #
 
         info = f"Score: {self.score}   Lives: {self.player.lives}"
